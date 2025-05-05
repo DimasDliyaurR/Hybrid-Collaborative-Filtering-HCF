[FIX] Evaluation Calculation

Evaluation/NDCG.py
@@ -1,28 +1,33 @@
 import numpy as np
 from MatrixRating import MatrixRating
-from prediction import Prediction
 from operator import itemgetter
 
-class NDCG() :
+class NDCG(MatrixRating) :
 
-    def __init__(self,data : MatrixRating, prediction : Prediction,*, N : int) -> None:
-        self.data = data
+    def __init__(self,data, top_n : list[list[list[int]]],*,path_evaluation : str = None,toyData : bool, N : int = 30) -> None:
+        MatrixRating.__init__(self,data,toyData=toyData)
+
         self.N = N
-        self.prediction = prediction
+        self.top_n = top_n
+        self.result_evaluation = self.main_calculation()
 
-    def groundTruth(self, u, indexTrain) -> np.array :
-         ground_truth = itemgetter(*self.data.getItem(u,indexTrain=indexTrain))(self.test[u])
 
-         return np.array([1 if self.prediction.get_top_n_specific_user(u,indexTrain)[:i+1][-1] in ground_truth else 0 for i in range(self.N) if len(ground_truth) > i])
+    def get_top_n_specific_user(self,u,*,indexTrain : int|None = None) -> list[float] :
+        return self.top_n[indexTrain][u]
+
+    def __groundTruth(self, u, indexTrain : int|None = None) -> np.array :
+         data_test = self.getItemTest(u,indexTrain=indexTrain)
+         return np.array([1 if (self.get_top_n_specific_user(u,indexTrain=indexTrain)[:i+1][-1] in data_test) and len(data_test) > i else 0 for i in range(self.N)])
 
     def IDCG(self) -> float :
-        return sum(1/np.log2(np.arange(2,self.N+1))).real
+        return sum(1/np.log2(np.arange(2,self.N+2))).real
 
-    def DCG(self, u) -> float :
-        return sum(self.groundTruth(u)/np.log2(np.arange(2,self.N+1))).real
+    def DCG(self, u, indexTrain : int|None = None) -> float :
+        return sum(self.__groundTruth(u,indexTrain=indexTrain)/np.log2(np.arange(2,self.N+2))).real
 
-    def NDCG(self,u) -> float :
-        return self.DCG(self.N,u) / self.IDCG(self.N)
+    def NDCG(self,u,indexTrain) -> float :
+        print("DCG = ",self.DCG(u,indexTrain=indexTrain),"IDCG = ",self.IDCG())
+        return self.DCG(u,indexTrain=indexTrain) / self.IDCG()
     
     def main_calculation(self) -> float :
         """
@@ -40,18 +45,23 @@ class NDCG() :
         
         """
         result_per_fold = []
-        for indexTrain in range(len(self.data.train)) :
+        for indexTrain in range(len(self.train)) :
             
             number_of_evaluation_per_fold = 0
-            for u in range(len(self.data.train[indexTrain])) :
+            for u in range(len(self.train[indexTrain])) :
                 # Proses Tahap 1
-                number_of_evaluation_per_fold += self.NDCG(u)
+                number_of_evaluation_per_fold += self.NDCG(u,indexTrain)
             
             # Proses Tahap 2
-            mean_evaluation_per_fold = number_of_evaluation_per_fold/self.data.numberOfUser
+            print("Tahap 1 =", number_of_evaluation_per_fold)
+            mean_evaluation_per_fold = number_of_evaluation_per_fold/self.numberOfUser
             result_per_fold.append(mean_evaluation_per_fold)
         
         # Proses Tahap 3
-        total_mean_evaluation = sum(result_per_fold)/len(self.data.train)
+        print("Tahap 2 =", result_per_fold)
+        total_mean_evaluation = sum(result_per_fold)/len(self.train)
+        print("Tahap 3 =", total_mean_evaluation)
         return total_mean_evaluation
-        
+    
+    def show_evaluation(self) :
+        return self.result_evaluation

MatrixRating/MatrixRating.py
@@ -32,22 +32,28 @@ class MatrixRating():
             if type(data) is list :
                 raise ValueError(f"{self} data should be type str")
             
+            # Keseuluruhan Dataset
             self.dataset = pd.read_csv(f"{self.data}/u.data",sep="\t", names=["user_id","item_id","rating","timestamp"])
-            self.numberOfUserAndItem = self.__getNumberOfUserAndItemData()            
-            self.numberOfUser = self.numberOfUserAndItem["users"]
-            self.numberOfItem = self.numberOfUserAndItem["items"]
             
-            self.maxIdOfUserAndItem = self._getMaxOfIdUserAndItemData()            
-            self.maxIdOfUser = self.numberOfUserAndItem["users"]
-            self.maxIdOfItem = self.numberOfUserAndItem["items"]
+            numberOfUserAndItem = self.__getNumberOfUserAndItemData()            
+            self.numberOfUser = numberOfUserAndItem["users"]
+            self.numberOfItem = numberOfUserAndItem["items"]
+            
+            self.maxIdOfUserAndItem = self.__getMaxOfIdUserAndItemData()            
+            self.maxIdOfUser = numberOfUserAndItem["users"]
+            self.maxIdOfItem = numberOfUserAndItem["items"]
 
             self.train = [self.__processDataTrain(i) for i in range(1,6)]
             self.test = [self.__processDataTest(i) for i in range(1,6)]
+
             self.train_transpose = [np.transpose(matrix) for matrix in self.train]
+            self.test_transpose = [np.transpose(matrix) for matrix in self.test]
 
         
         self.__matrix_component_of_item = self.__dozenOfItem()
         self.__matrix_component_of_user = self.__dozenOfUser()
+        self.__matrix_component_of_item_test = self.__dozenOfItemTest()
+        self.__matrix_component_of_user_test = self.__dozenOfUserTest()
 
     def __dozenOfItem(self) -> dict :
         if self.toyData :
@@ -72,14 +78,33 @@ class MatrixRating():
             "unrated" : [[[ j for j in range(len(self.train_transpose[indexTrain][i])) if self.train_transpose[indexTrain][i][j] == 0] for i in range(len(self.train_transpose[indexTrain]))] for indexTrain in range(len(self.train))],
             "rated" : [[[ j for j in range(len(self.train_transpose[indexTrain][i])) if self.train_transpose[indexTrain][i][j] != 0] for i in range(len(self.train_transpose[indexTrain]))] for indexTrain in range(len(self.train))],
         }
+    
+    def __dozenOfItemTest(self) -> dict :
+
+        return {
+            "unrated" : [[[ j for j in range(len(self.test[indexTrain][i])) if self.test[indexTrain][i][j] == 0] for i in range(len(self.test[indexTrain]))] for indexTrain in range(len(self.test))],
+            "rated" : [[[ j for j in range(len(self.test[indexTrain][i])) if self.test[indexTrain][i][j] != 0] for i in range(len(self.test[indexTrain]))] for indexTrain in range(len(self.test))],
+        }
+
+    def __dozenOfUserTest(self) -> dict[str : Matrix ] :
+        if self.toyData :
+            return {
+                "unrated" : [[j for j in range(len(self.reverseMatrixRating[0])) if self.reverseMatrixRating[i][j] == 0 ] for i in range(len(self.reverseMatrixRating))],
+                "rated" : [[j for j in range(len(self.reverseMatrixRating[0])) if self.reverseMatrixRating[i][j] != 0 ] for i in range(len(self.reverseMatrixRating))]
+            }
+        
+        return {
+            "unrated" : [[[ j for j in range(len(self.test_transpose[indexTrain][i])) if self.test_transpose[indexTrain][i][j] == 0] for i in range(len(self.test_transpose[indexTrain]))] for indexTrain in range(len(self.test))],
+            "rated" : [[[ j for j in range(len(self.test_transpose[indexTrain][i])) if self.test_transpose[indexTrain][i][j] != 0] for i in range(len(self.test_transpose[indexTrain]))] for indexTrain in range(len(self.test))],
+        }
 
     def __getNumberOfUserAndItemData(self) -> dict[int] :        
         return {
             "users" : len(pd.unique(self.dataset["user_id"])),
             "items" : len(pd.unique(self.dataset["item_id"]))
             }
-    
-    def _getMaxOfIdUserAndItemData(self) -> dict[int] :        
+
+    def __getMaxOfIdUserAndItemData(self) -> dict[int] :  
         return {
             "users" : max(pd.unique(self.dataset["user_id"])),
             "items" : max(pd.unique(self.dataset["item_id"]))
@@ -135,6 +160,56 @@ class MatrixRating():
             if indexTrain is None :
                 raise ValueError(f"Index of train is missing {self}")
             return self.__matrix_component_of_user[label][indexTrain][item]
+    
+    def getItemTest(self, user : int, indexTrain : int|None = None,*, interacted : bool = True) -> Vector :
+        """
+        Get set of item have rated by specific user
+            Representation of I_u or \widehat{I}_u (depend on parameter interacted) Notation
+        ------------------------------------------------------------------------------------
+        Parameters :
+            user : int 
+                specific user 
+
+            interacted : bool 
+                The item have rated or not the item
+
+        Returns :
+            list[float] : Set of item
+        """
+
+        label = "rated" if interacted else "unrated"
+
+        if self.toyData :
+            return self.__matrix_component_of_item_test[label][user]
+        else :
+            if indexTrain is None :
+                raise ValueError(f"Index of train is missing {self}")
+            return self.__matrix_component_of_item_test[label][indexTrain][user]
+
+    def getUserTest(self ,item : int,indexTrain : int|None = None,*,interacted: bool=True) -> Vector :
+        """
+        Get set of user have rated by specific item
+            Representation of U_i or \widehat{U}_i (depend on parameter interacted) Notation
+        ------------------------------------------------------------------------------------
+        Parameters
+        ----------
+            item : int
+                specific item
+            interacted : bool
+                The item already rated or not by user
+
+        Returns
+        -------
+            list[float] : Set of item
+        """
+        label = "rated" if interacted else "unrated"
+        
+        if self.toyData :
+            return self.__matrix_component_of_user_test[label][item]
+        else :
+            if indexTrain is None :
+                raise ValueError(f"Index of train is missing {self}")
+            return self.__matrix_component_of_user_test[label][indexTrain][item]
 
     def getItemWithValue(self,user : int, *,indexTrain : int|None = None, interacted : bool = True) -> Vector :
         if self.toyData :
@@ -203,3 +278,6 @@ class MatrixRating():
             Pandas
         """
         return pd.DataFrame(self.train[u])
+    
+    def show_dataset(self):
+        return self.transformationData(self.dataset)